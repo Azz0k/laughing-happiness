f97c9565c00a8e8c037764b6d7aca2dc613009b2
Pavel Milokum <milokumpavel@gmail.com>
2025-07-23 22:16:06 +0300

    ip calc shows network and broadcast

--- src/components/IpCalculator.tsx
+++ src/components/IpCalculator.tsx
@@ -1,25 +1,42 @@
 import { observer } from "mobx-react";
 import {Input, Button, Radio} from "antd";
+import {globalState} from "../stores/GlobalState.tsx";
+import {Address4} from "ip-address";
 
 const IpCalculator = observer(()=> {
-    const radioButtons = Array.from({ length:32 }, (_, i) =>
-        <Radio.Button value={i}>
+    const radioButtons = Array.from({ length:31 }, (_, i) =>
+        <Radio.Button value={i} key={i}>
             /{i}
         </Radio.Button>
-
     );
+    let networkAddress = "";
+    let broadcastAddress = "";
+    if (globalState.ipCalcValidated) {
+        const ipAddressObject = new Address4(`${globalState.ipCalcAddress}/${globalState.ipCalcMask}`);
+        networkAddress = ipAddressObject.startAddress().address;
+        broadcastAddress = ipAddressObject.endAddress().address;
+    }
     return(
         <>
             <div className={"flex flex-wrap gap-10 p-5"}>
                 <div className="w-1/3 min-w-[200px]">
-                    <Input placeholder="Введи ip адрес" maxLength={20}/>
+                    <Input placeholder="Введи ip адрес"
+                           maxLength={15}
+                           value={globalState.ipCalcAddress}
+                           onChange={(e)=>globalState.handleIpCalcAddressChange(e.target.value)}
+                    />
                 </div>
                 <span className="w-1/5 min-w-[120px] self-center">Выбери подсеть: </span>
-                <Radio.Group defaultValue={24} >
+                <Radio.Group
+                    value={globalState.ipCalcMask}
+                    onChange={(e)=>globalState.handleIpCalcMaskSelect(e.target.value)}
+                >
                     {radioButtons}
                 </Radio.Group>
             </div>
-            <Button >Calculate</Button>
+            <Button onClick={globalState.validateIpCalcAddress}>Calculate</Button>
+            {globalState.ipCalcValidated && <p>Network: {networkAddress}</p>}
+            {globalState.ipCalcValidated && <p>Broadcast: {broadcastAddress}</p>}
         </>
     );
 });
--- src/stores/GlobalState.tsx
+++ src/stores/GlobalState.tsx
@@ -1,14 +1,33 @@
 import { makeAutoObservable } from "mobx";
 
+
 class GlobalState {
   public test: string | null = "mobx";
   public menuKeySelected = '1';
+  public ipCalcMask = 24;
+  public ipCalcAddress: string = "";
+  public ipCalcValidated:boolean = false;
+
   constructor() {
     makeAutoObservable(this);
   }
   handleSelectMenuItem(key:string) {
     this.menuKeySelected = key;
-    console.log(this.menuKeySelected);
+  }
+  handleIpCalcMaskSelect(mask:number){
+    this.ipCalcMask = mask;
+  }
+  handleIpCalcAddressChange(address:string){
+    this.ipCalcValidated = false;
+    const lastAddressSymbol = address.length - 1;
+    if (address.length>0 && !("0123456789.,".includes(address[lastAddressSymbol])))
+      return;
+    address = address.replaceAll(",",".");
+    this.ipCalcAddress = address.trim();
+  }
+  validateIpCalcAddress = ()=>{
+    const pattern =/^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/;
+    this.ipCalcValidated = pattern.test(this.ipCalcAddress);
   }
 
 }
