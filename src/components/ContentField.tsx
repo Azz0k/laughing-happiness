98434d4b5f63970e5c61b38c6c54f9f925d618ea
Pavel Milokum <milokumpavel@gmail.com>
2025-07-24 08:15:59 +0300

    menu rendering selection

--- src/components/ContentField.tsx
+++ src/components/ContentField.tsx
@@ -1,10 +1,20 @@
 import {observer} from "mobx-react";
 import {IpCalculator} from "./IpCalculator.tsx";
+import {globalState} from "../stores/GlobalState.tsx";
 
 const ContentField = observer(()=>{
+    let SelectedElement;
+    switch (globalState.menuKeySelected){
+        case '1.1':
+            SelectedElement = <IpCalculator />;
+            break;
+        default:
+            SelectedElement = null;
+    }
+
    return(
        <>
-           <IpCalculator />
+           {SelectedElement}
        </>
    )
 });
